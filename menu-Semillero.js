b3afebc99d1e47243ac54f48edf04bb7d1783867
Edinson Erraez <erraez.edinson@gmail.com>
2022-01-06 20:36:51 -0500

    se creo el menu semillero con la informacion de Edinson Erraez

---
+++ menu-Semillero.js
@@ -0,0 +1,19 @@
+{
+    "COMMONS": {
+        "MENU": {
+            "MNU_EEE_BVDA": "Bienvenida Edinson Erraez",
+            "MNU_SMCMC": "Formularios Semillero",
+            "MNU_EEE_CALIFICACIONES": "Calificaciones Edinson Erraez",
+            "MNU_EEE_CALCULADORA": "Calculadora Edinson Erraez",
+            "MNU_EEE_EVA2": "Evaluacion 2 Edinson Erraez",
+            "MNU_EEE_CREAR_CLIENTE": "Crear Cliente Edinson Erraez",
+            "MNU_EEE_FORMULARIO_TIENDA": "Formulario tienda Edinson Erraez",
+            "MNU_EEE_BUSCAR_CLIENTE": "Buscar Cliente Edinson Erraez",
+            "MNU_EEE_RECUPERAR_CLIENTES": "Recuperar Clientes Edinson Erraez",
+            "MNU_EDINSON_ERRAEZ": "Evaluacion Formularios Edinson Erraez",
+            "MNU_EEE_EVALUACION_PRODUCTO": "Formulario Crear Producto",
+            "MNU_EEE_EVALUACION_PRODUCTOS": "Listar Productos",
+            "MNU_EEE_EVALUACION_BUSCAR_CLIENTES": "Buscar Clientes"
+        }
+    }
+}
