c046cdece17a6e5d307944573d2c79181afb3efd
Amelia Labanda <amelialabanda94@gmail.com>
2022-01-07 13:51:25 -0500

    Se agregan menus ALF

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -20,7 +20,18 @@
 			"MNU_NDQS_EVA_2": "Evaluacion 2",
 			"MNU_NDQS_BLIS": "Test BLIS",
 			"MNU_NDQS_Tienda": "Tienda BLIS",
-			"MNU_NDQS_Grid": "GRID Clientes"
+			"MNU_NDQS_Grid": "GRID Clientes",
+            "MNU_ALF_BVDA":"Bienvenida Amelia Labanda",
+			"MNU_ALF_CALIFICACIONES":"Calificaciones",
+			"MNU_ALF_CALCULADORA":"Calculadora",
+			"MNU_ALF_CLIENTE":"Crear Cliente",
+			"MNU_ALF_CLIENTE_BUSCAR":"Buscar Clientes",
+			"MNU_ALF_CLIENTE_LISTA":"Listar Clientes",
+			"MNU_ALF_TIENDA":"Listar Tiendas",
+			"MNU_ALF_FACTURACION":"Facturacion ALF",
+			"MNU_ALF_FAC_CREAR":"Crear Producto",
+			"MNU_ALF_FAC_LISTAR":"Listar Productos",
+			"MNU_ALF_FAC_BUSCAR":"Buscar Cliente"
         }
     }
 }
