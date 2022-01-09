f222813e9153faaa1f52f9585317fdf9b750caee
Ayrton Adame <ayrton.adame94@gmail.com>
2022-01-09 14:31:29 -0500

    Codificado del lado del Servidor, Form AFAB ... añadido el Jar y Tag respectivo

--- semillero-backend/TAGS/menu-GITCL-es_EC.js
+++ semillero-backend/TAGS/menu-GITCL-es_EC.js
@@ -5,7 +5,8 @@
 			"MNU_REPOSITORIO_AAEN": "Practica Repositorio",
 			"MNU_REPOSITORIO_AAEN_NDQS": "Sub Menu Nixon",
 			"MNU_REPOSITORIO_AAEN_ALLF": "Sub Menu Amelia",
-			"MNU_REPOSITORIO_AAEN_EEEV": "Sub Menu Edinson"
+			"MNU_REPOSITORIO_AAEN_EEEV": "Sub Menu Edinson",
+			"MNU_REPOSITORIO_AAEN_AFAB": "Sub Menu Ayrton"
 		}
 	}
 }
